a61f59474328ed2bcfeceb4f1d54267b33baf18c
mercymugambi <mercymugambi2016@gmail.com>
2023-04-28 23:27:16 +0300

    Move quote to main file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,10 +1,8 @@
 import CalculatorButtons from './CalculatorButtons';
-import Quote from './Quote';
 
 function Calculator() {
   return (
     <div>
-      <Quote />
       <div className="calculator">
         <form action="">
           <CalculatorButtons />
