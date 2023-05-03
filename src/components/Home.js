ff76823e2ca298b80e9634bef08a94024d8635a2
mercymugambi <mercymugambi2016@gmail.com>
2023-05-03 23:04:51 +0300

    Add route to the App.js

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+const Home = () => (
+  <>
+    <h2>Welcome to our page!</h2>
+    <p>
+      Welcome to Math Magician, your one-stop-shop for quick mental
+      math practice and inspirational quotes. Sharpen your arithmetic
+      skills by solving our daily challenges, or simply enjoy a moment
+      of reflection with a new quote every day. Whether you are a seasoned
+      mathematician or just getting started, Math Magician is the perfect place
+      to keep your mind sharp and your spirit inspired.
+    </p>
+  </>
+);
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
